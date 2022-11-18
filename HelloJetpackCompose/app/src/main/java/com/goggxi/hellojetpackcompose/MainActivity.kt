2ba1aae41243077bd66d4526a4d3dac936921c3e
goggxi <goggxi@gmail.com>
2022-11-18 20:30:00 +0800

    add hello jetpack compose project

---
+++ HelloJetpackCompose/app/src/main/java/com/goggxi/hellojetpackcompose/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.goggxi.hellojetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.goggxi.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloJetpackComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(
+    name = "single preview",
+    showBackground = true,
+    group = "greeting",
+)
+
+@Preview(
+    name = "full screen preview",
+    showBackground = true,
+    group = "greeting",
+    showSystemUi = true,
+    device = Devices.PIXEL_3A,
+)
+
+@Composable
+fun DefaultPreview() {
+    HelloJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
